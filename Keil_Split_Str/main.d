90b3c02f47659afb65dd16a8f79f240b8dc115bb
flavius-aetios <mihail25.98@gmail.com>
2019-03-12 12:55:06 +0800

    New Keil project

--- Keil_Split_Str/main.d
+++
@@ -1,3 +0,0 @@
-.\main.o: main.cpp
-.\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-.\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
--- Keil_Split_Str/system_mdr32f9qx.d
+++
@@ -1,8 +0,0 @@
-.\system_mdr32f9qx.o: RTE\Device\MDR1986BE92\system_MDR32F9Qx.c
-.\system_mdr32f9qx.o: C:\Keil_v5\ARM\PACK\Keil\MDR1986BExx\1.5\Libraries\CMSIS\CM3\DeviceSupport\MDR32F9Qx\inc\MDR32Fx.h
-.\system_mdr32f9qx.o: C:\Keil_v5\ARM\PACK\Keil\MDR1986BExx\1.5\Libraries\CMSIS\CM3\CoreSupport\core_cm3.h
-.\system_mdr32f9qx.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\system_mdr32f9qx.o: C:\Keil_v5\ARM\PACK\Keil\MDR1986BExx\1.5\Libraries\CMSIS\CM3\CoreSupport\core_cmInstr.h
-.\system_mdr32f9qx.o: C:\Keil_v5\ARM\PACK\Keil\MDR1986BExx\1.5\Libraries\CMSIS\CM3\CoreSupport\core_cmFunc.h
-.\system_mdr32f9qx.o: .\RTE\Device\MDR1986BE92\system_MDR32F9Qx.h
-.\system_mdr32f9qx.o: C:\Keil_v5\ARM\PACK\Keil\MDR1986BExx\1.5\Config\MDR32F9Qx_config.h
